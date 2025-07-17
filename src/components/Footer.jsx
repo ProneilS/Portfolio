7179b96178ded81b1f0656aab2812775c8530e34
ProneilS <proneil.sengutpa@gmail.com>
2025-07-17 17:06:47 +0530

    Fix footer invalid nested p tags

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,10 @@ export const Footer = () => {
       <p className="text-sm text-muted-foreground">
         {" "}
         &copy; {new Date().getFullYear()} Pedrotech.co. All rights reserved.
+        &copy; {new Date().getFullYear()}{" "}
+        <span className="text-sm text-gray-500">
+          Crafted in the dark by <span className="font-bold">Proneil</span>
+        </span>
       </p>
       <a
         href="#hero"
