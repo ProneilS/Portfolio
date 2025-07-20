c1c0a2e99c7609789934703a559435658c5c97f7
ProneilS <proneil.sengutpa@gmail.com>
2025-07-20 09:39:09 +0530

    Updated resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/resume.docx" target="_blank" rel="noopener noreferrer"
+                href="/resume.pdf" target="_blank" rel="noopener noreferrer"
 
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
